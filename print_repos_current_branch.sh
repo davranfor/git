a4690085d05519176044f4e96e7944c9336a03d3
David Ranieri <50286622+davranfor@users.noreply.github.com>
2022-02-01 20:41:08 +0100

    Add files via upload

--- print_repos_current_branch.sh
+++ print_repos_current_branch.sh
@@ -4,12 +4,20 @@
 
 source $(dirname $0)/config
 
-red='\033[0;31m'
-noc='\033[0m' # No Color
+red='\033[0;31m'    # Color red
+def='\033[0m'       # No color
+
+if [ -t 1 ]; then
+    # Running from a terminal
+    fmt="${red}%s${def}"
+else
+    # Running through a pipe
+    fmt="%s"
+fi
 
 find $workdir -type d -name '.git' -print0 2>/dev/null | sort -z | while read -d $'\0' folder; do
     cd $folder
-    printf "${red}%s${noc} " $(sed 's:.*/::' <<< ${folder::-5})
+    printf "$fmt " $(sed 's:.*/::' <<< ${folder::-5})
     git rev-parse --abbrev-ref HEAD
 done
 
